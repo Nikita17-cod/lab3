dcc7253c7e594c0e0922e465c11abc71c992e656
nikitakoshyk <64362350+Nikita17-cod@users.noreply.github.com>
2025-05-30 00:18:28 +0300

    Update tests.py

--- store/tests.py
+++ store/tests.py
@@ -1,1 +1,8 @@
+from django.test import TestCase
+from .models import Product
+
+class ProductTestCase(TestCase):
+    def test_create_product(self):
+        product = Product.objects.create(name="Test", description="desc", price=99.99)
+        self.assertEqual(product.name, "Test")
 
